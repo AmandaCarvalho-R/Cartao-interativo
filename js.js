233b53918925004c06465dd3b1b7c04c1198be2b
Amanda Carvalho <amanda.chrys370@hotmail.com>
2023-01-12 21:48:48 -0400

    iniciando js

---
+++ js.js
@@ -0,0 +1,8 @@
+    var nometitular = document.querySelector(nometitular)
+<script>
+    
+
+function nometitular(params) {
+    document.getElementById("nometitular").value = nometitular
+}
+</script>
